bd322ec4c871d4a48c705f5c33aaa69a71fc9cb1
MayurKry <mayur.thakkarkry@gmail.com>
2025-12-16 10:12:35 +0530

    feat: Add Folder model, extend Express Request type, and include a standard .gitignore.

--- backend/src/models/folder.model.ts
+++ backend/src/models/folder.model.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
 
 export interface IFolder extends Document {
+    _id: Types.ObjectId;
     name: string;
     parentId?: Types.ObjectId;
     userId: Types.ObjectId;
